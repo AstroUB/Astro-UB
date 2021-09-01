0ba71a85a21cdf42e7abe2827d10ec2167912df3
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-09-01 10:38:56 +0530

    •Astro•

--- Astrorun.py
+++ Astrorun.py
@@ -1,7 +1,7 @@
 from os import system
 
 system(
-      "git clone https://gitlab.com/loverboyXD/astroub.git && cd astroub && python3 -m astro"
+      "git clone https://gitlab.com/loverboyXD/AstroUB.git && cd AstroUB && python3 -m astro"
 )
 # if something is wrong 
 print("You Have Got Error While Deployment Report and Get Help from @Astro_HelpChat")
