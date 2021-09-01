9427c9886971f27cf6518b11df5e978147ccb04f
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-09-01 10:22:42 +0530

    •Astro•

---
+++ Astrorun.py
@@ -0,0 +1,10 @@
+from os import system
+
+system(
+      "git clone https://gitlab.com/loverboyXD/astroub.git && cd astroub && python3 -m astro"
+)
+# if something is wrong 
+print("You Have Got Error While Deployment Report and Get Help from @Astro_HelpChat")
+# Script written for stop heroku suspension 
+# To Stop app creation failed problem BY @CriminaL786
+ 
